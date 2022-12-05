551045c71f2e5fa1f3cb4b77e162556b8b6e0c31
zakaria-shahen <zakariay.shahen@gmail.com>
2022-12-05 20:36:57 +0200

    init

---
+++ src/main/java/com/mycompany/firstspringconfigurationserver/FirstSpringConfigurationApplication.java
@@ -0,0 +1,15 @@
+package com.mycompany.firstspringconfigurationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class FirstSpringConfigurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringConfigurationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mycompany/firstspringconfigurationserver/FirstSpringConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.firstspringconfigurationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringConfigurationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
